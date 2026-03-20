daa70577d8926f2a4788de9ee7c556b6a822b64c
Eric <1fortesting123@gmail.com>
2026-03-20 22:33:28 +0000

    let the intensity of the heartbeat animation match the seconds of the co

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,17 +94,16 @@ export default {
         },
         'heartbeat': {
           '0%': { transform: 'scale(1)' },
-          '14%': { transform: 'scale(1.2)' },
-          '28%': { transform: 'scale(1)' },
-          '42%': { transform: 'scale(1.2)' },
-          '70%': { transform: 'scale(1)' },
+          '20%': { transform: 'scale(1.3)' },
+          '40%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(1)' },
         }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'pulse-second': 'pulse-second 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'heartbeat': 'heartbeat 1.2s ease-in-out infinite',
+        'heartbeat': 'heartbeat 1s ease-in-out infinite',
       },
     },
   },
