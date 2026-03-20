6fccc2746b29215a356d6a88fbaba226a84f3240
Eric <1fortesting123@gmail.com>
2026-03-20 22:35:24 +0000

    reduce the pop up intensity, keep the beating timing 1 beat per second

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,7 +94,7 @@ export default {
         },
         'heartbeat': {
           '0%': { transform: 'scale(1)' },
-          '20%': { transform: 'scale(1.3)' },
+          '20%': { transform: 'scale(1.15)' },
           '40%': { transform: 'scale(1)' },
           '100%': { transform: 'scale(1)' },
         }
