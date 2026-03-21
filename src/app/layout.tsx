76ce1c6d0d7cac06a0ee5703ba9da95c7f893d63
Eric <1fortesting123@gmail.com>
2026-03-21 23:51:22 +0000

    i am adding tawk.io assistant so put this in the code <!--Start of Tawk.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,12 @@ export default function RootLayout({
             <GlobalToaster />
           </Suspense>
         </ThemeProvider>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/69bf2d332273861c39a78f91/1jk9ch58q';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();`,
+          }}
+        />
       </body>
     </html>
   );
