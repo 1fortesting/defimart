6955627b10ec59550694e2e44ce66c4dd5b95da8
Eric <1fortesting123@gmail.com>
2026-03-22 06:57:19 +0000

    for the tawk.io icon, it has overlap the profile icon in the mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
-            __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/69bf2d332273861c39a78f91/1jk9ch58q';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();`,
+            __html: `var Tawk_API=Tawk_API||{}; if(window.innerWidth < 768){Tawk_API.customStyle={visibility:{mobile:{position:'br',yOffset:90}}}};var Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/69bf2d332273861c39a78f91/1jk9ch58q';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();`,
           }}
         />
       </body>
