18db67b76f12e37fa9df6f47fe4e01940677a4bd
Eric <1fortesting123@gmail.com>
2026-03-29 12:53:03 +0000

    weve got some changes to do, firstly make light mode the main mode, so d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <StorefrontShell header={<Header />} bottomNav={<BottomNav />}>
